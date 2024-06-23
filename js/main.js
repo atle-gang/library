6dac806c9e1bf3874f98951167d2d1ea1f8caf6a
Atlegang Sethono <atlegangcode@gmail.com>
2024-06-23 17:31:40 +0200

    Initial push

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 import { Library } from "./library";
 import { Book } from "./book";
+
+const title = prompt("Name: ");
+const author = prompt("Author: ");
+const pages = prompt("Pages: ");
+const publishedYear = prompt("Year: ");
+
